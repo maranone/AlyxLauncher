d705f18f78d4455c8f400344e510f2503ce32698
maranone <39731080+maranone@users.noreply.github.com>
2020-04-16 19:04:41 +0200

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -6,14 +6,16 @@ Public Class Form1
     Dim difolder As String
     Dim config_file As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
             didi = FolderBrowserDialog1.SelectedPath & "\steamapps\common\Half-Life Alyx\game\hlvr\maps"
             difolder = FolderBrowserDialog1.SelectedPath
             Dim di As DirectoryInfo = New DirectoryInfo(didi)
             If di.Exists Then
                 Dim finfo As New IO.DirectoryInfo(didi)
                 For Each fi In finfo.GetFiles
-                    ListBox1.Items.Add(fi.Name)
+                    If UCase(fi.Name.Substring(fi.Name.Length - 3, 3)) = "VPK" Then
+                        ListBox1.Items.Add(fi.Name)
+                    End If
                 Next
                 config_file = ".\AlyxLauncher.txt"
                 Dim objWriter As New System.IO.StreamWriter(config_file, False)
@@ -32,6 +34,9 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Button2.Enabled = False
+        CheckBox1.Checked = True
+        CheckBox2.Checked = True
+        CheckBox3.Checked = True
         config_file = ".\AlyxLauncher.txt"
         If File.Exists(config_file) Then
             config_file = ".\AlyxLauncher.txt"
@@ -43,7 +48,9 @@ Public Class Form1
             If di.Exists Then
                 Dim finfo As New IO.DirectoryInfo(didi)
                 For Each fi In finfo.GetFiles
-                    ListBox1.Items.Add(fi.Name)
+                    If UCase(fi.Name.Substring(fi.Name.Length - 3, 3)) = "VPK" Then
+                        ListBox1.Items.Add(fi.Name)
+                    End If
                 Next
             End If
         Else
@@ -52,7 +59,9 @@ Public Class Form1
             If di.Exists Then
                 Dim finfo As New IO.DirectoryInfo(didi)
                 For Each fi In finfo.GetFiles
-                    ListBox1.Items.Add(fi.Name)
+                    If UCase(fi.Name.Substring(fi.Name.Length - 3, 3)) = "VPK" Then
+                        ListBox1.Items.Add(fi.Name)
+                    End If
                 Next
                 config_file = ".\AlyxLauncher.txt"
                 Dim objWriter As New System.IO.StreamWriter(config_file, False)
@@ -64,9 +73,16 @@ Public Class Form1
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim TexttoRun As String
+        TexttoRun = TextBox1.Text
+        If CheckBox1.Checked = True Then TexttoRun = TexttoRun & " " & TextBox2.Text
+        If CheckBox2.Checked = True Then TexttoRun = TexttoRun & " " & TextBox3.Text
+        If CheckBox3.Checked = True Then TexttoRun = TexttoRun & " " & TextBox4.Text
+        If CheckBox4.Checked = True Then TexttoRun = TexttoRun & " " & TextBox5.Text
         Dim objbat As New System.IO.StreamWriter(".\AlyxLauncher.bat", False)
-        objbat.WriteLine(textbox1.text)
+        objbat.WriteLine(TexttoRun)
         objbat.Close()
         Shell(".\AlyxLauncher.bat", vbNormalFocus)
     End Sub
+
 End Class
