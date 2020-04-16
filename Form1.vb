9477d3d8c52fb6bacd7aac5b59a6b31e76a923ee
maranone <39731080+maranone@users.noreply.github.com>
2020-04-16 11:05:02 +0200

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -26,10 +26,12 @@ Public Class Form1
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
         dimap = difolder & "\steamapps\common\Half-Life Alyx\game\bin\win64\hlvr.exe"
         Dim A = ListBox1.SelectedItem.ToString()
-        TextBox1.Text = Chr(34) & dimap & Chr(34) & " +map " & A.Substring(0, A.Length - 4)
+        TextBox1.Text = Chr(34) & dimap & Chr(34) & " -console –vconsole +map " & A.Substring(0, A.Length - 4)
+        Button2.Enabled = True
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Button2.Enabled = False
         config_file = ".\AlyxLauncher.txt"
         If File.Exists(config_file) Then
             config_file = ".\AlyxLauncher.txt"
@@ -44,7 +46,21 @@ Public Class Form1
                     ListBox1.Items.Add(fi.Name)
                 Next
             End If
+        Else
+            didi = "C:\Program Files (x86)\Steam"
+            Dim di As DirectoryInfo = New DirectoryInfo(didi)
+            If di.Exists Then
+                Dim finfo As New IO.DirectoryInfo(didi)
+                For Each fi In finfo.GetFiles
+                    ListBox1.Items.Add(fi.Name)
+                Next
+                config_file = ".\AlyxLauncher.txt"
+                Dim objWriter As New System.IO.StreamWriter(config_file, False)
+                objWriter.WriteLine(didi)
+                objWriter.Close()
+            End If
         End If
+
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
